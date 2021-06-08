568bafb588f300b893c5a1b00d233a149b9ee13d
aswanibtech <aswanibtech@gmail.com>
2021-06-08 13:26:17 +0530

    ifelse intro

---
+++ farentodegree.py
@@ -0,0 +1,4 @@
+f=float(input("Enter the temperature in farenhight"))
+print("Given Temp=",f)
+c=(5*(f-32))/9
+print("Temp in degree=",c)
---
+++ ifelseexp.py
@@ -0,0 +1,28 @@
+age=int(input("enter your age"))
+print("Age=",age)
+if age>=18:
+    print("You are Elligible for Licence")
+else:
+    print("You are not elligible")
+
+
+
+
+
+
+'''
+if(condition)
+     {
+task to be excecuted if condition is true
+      }
+else
+{
+task to be excecuted if condition is false
+}
+
+if condition:
+    task to be excecuted if condition is true
+else:
+    task to be excecuted if condition is false   
+
+'''
