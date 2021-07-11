67f096436243ed19f185dbd64d5f0b974bfdf2c9
aswanibtech <aswanibtech@gmail.com>
2021-07-11 06:00:10 +0530

    project

--- databaseconnectexp.py
+++ databaseconnectexp.py
@@ -3,12 +3,12 @@ conn=pymysql.connect(host="localhost",user="root",db="mppython")
 print("connection established")
 mycursor=conn.cursor()
 #que="create table user_info(name varchar(50),age int(3))"
-#que="insert into user_info(name,age)values('Ram',22)"
-que="update user_info set name='Shyam' where name='Ram'"
+que="insert into user_info(name,age)values('Ram',22)"
+#que="update user_info set name='Shyam' where name='Ram'"
 mycursor.execute(que)
 conn.commit() # to save the result(data) of execution of any query
 #print("table Created Successfully")
-#print("Data Stored successfully")
-print("Data Updated  successfully")
+print("Data Stored successfully")
+#print("Data Updated  successfully")
 mycursor.close()
 conn.close()
